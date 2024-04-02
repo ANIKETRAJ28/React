cd4fe36104de9c5e47e8e940fb07ff97a6b6a7d1
Aniket <aniketraj28042003@gmail.com>
2024-04-03 00:39:24 +0530

    added logic for tic-tac-toe

--- 07_Game/Tic-Tac-Toe_Game/src/components/Card/Card.jsx
+++ 07_Game/Tic-Tac-Toe_Game/src/components/Card/Card.jsx
@@ -1,9 +1,15 @@
 import Icon from "../Icons/Icon"
 import './Card.css'
-function Card({iconName}) {
+function Card({onPlay, player, index}) {
+    let icon = <Icon/>
+    if(player == 'X') {
+        icon = <Icon name={'Cross'}/>
+    } else if(player == 'O') {
+        icon = <Icon name={'Circle'}/>
+    }
     return(
-        <div className="card">
-            <Icon name={iconName}/>
+        <div className="card" onClick={() => onPlay(index)}>
+            {icon}
         </div>
     )
 }
--- 07_Game/Tic-Tac-Toe_Game/src/components/Grid/Grid.jsx
+++ 07_Game/Tic-Tac-Toe_Game/src/components/Grid/Grid.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import Card from "../Card/Card";
 import './Grid.css'
 
 function Grid({numberOfCards}) {
+    const [turn, setTurn] = useState(true); // false -> X, true -> 0
+    const [board, setBoard] = useState(Array(numberOfCards).fill('')) // ["", "", "", "", "", ""]
+    function play(index) {
+        console.log("move played", index);
+        if(turn) {
+            board[index] = 'O';
+        } else {
+            board[index] = 'X';
+        }
+        setBoard([...board]);
+        setTurn(!turn);
+    }
     return(
-        <div className="grid">
-            {Array(numberOfCards).fill(<Card/>).map((el, idx) => {
-                return <Card iconName='' key={idx}/>
-                }
-            )}
-        </div>
+        <>
+        <h1 className="turn-highlight">Current Turn: {(turn) ? 'O' : 'X'}</h1>
+            <div className="grid">
+                {board.map((value, idx) => {
+                    return <Card onPlay={play} player={value} key={idx} index={idx}/>
+                    }
+                )}
+            </div>
+        </>
     )
 }
 
